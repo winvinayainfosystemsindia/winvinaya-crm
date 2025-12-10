ff386a225bf02bd9d06cc6f15252d3c4cd0acd2f
daran6255 <darandharani6255@gmail.com>
2025-12-10 21:42:34 +0530

    basic changes are done in both frontend and backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -56,8 +56,8 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.BACKEND_CORS_ORIGINS,
-    # allow_origins=["*"],
+    # allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
