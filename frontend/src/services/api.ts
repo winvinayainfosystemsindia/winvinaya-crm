cc9fa10e467385468caeec77b0c384f3ac0c27d7
daran6255 <darandharani6255@gmail.com>
2025-12-13 00:27:42 +0530

    Frontend - base url modified

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import authService from './authService';
 
 const api = axios.create({
-	baseURL: 'http://localhost:8000/api/v1',
+	baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
 	headers: {
 		'Content-Type': 'application/json',
 	},
