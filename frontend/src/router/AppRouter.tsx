0edc73adc2df800d42b0514c7d49864faefc0fc3
daran6255 <darandharani6255@gmail.com>
2025-12-25 15:18:21 +0530

    Frontend - Sourcing Analytics import is removed

--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -14,7 +14,7 @@ import NotFoundPage from '../pages/common/NotFoundPage';
 import MaintenancePage from '../pages/common/MaintenancePage';
 import DocumentCollectionList from '../pages/documents/DocumentCollectionList';
 import DocumentCollection from '../pages/documents/DocumentCollection';
-const SourcingAnalytics = React.lazy(() => import('../pages/analytics/SourcingAnalytics'));
+// const SourcingAnalytics = React.lazy(() => import('../pages/analytics/SourcingAnalytics'));
 
 const AppRouter: React.FC = () => {
 	return (
@@ -37,7 +37,7 @@ const AppRouter: React.FC = () => {
 						<Route path="candidates">
 							<Route index element={<CandidateList />} />
 							<Route path="list" element={<CandidateList />} />
-							<Route path="overview" element={<SourcingAnalytics />} />
+							{/* <Route path="overview" element={<SourcingAnalytics />} /> */}
 							<Route path="screening" element={<ScreeningList />} />
 							<Route path="counseling" element={<CounselingList />} />
 							<Route path="documents" element={<DocumentCollectionList />} />
