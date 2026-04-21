824dfc4f6d5eb9b2d9d5e8d3f92a91d1c0888b0d
daran6255 <darandharani6255@gmail.com>
2026-04-21 21:09:30 +0530

    unused component removed

--- frontend/src/components/placement/jobroles/details/mapping/index.ts
+++ frontend/src/components/placement/jobroles/details/mapping/index.ts
@@ -1,4 +1,3 @@
 export { default as SuggestionsTable } from './SuggestionsTable';
 export { default as MappedCandidatesList } from './MappedCandidatesList';
-export { default as MappingDialog } from './MappingDialog';
 export { default as BulkMappingDialog } from './BulkMappingDialog';
